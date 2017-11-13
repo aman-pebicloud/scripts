1b43ce14aade6ddc68d71b30f6e2e2e91e4e3a20
Aman <amanullahm@wavity.com>
2017-11-13 18:30:05 +0530

    updating scripts to copy tenant and open self.wavity.net

--- bin/buildDistributions.sh
+++ bin/buildDistributions.sh
@@ -5,9 +5,9 @@ echo "gradle clean build"
 gradle clean build
 cd distribution/build/distributions
 echo "----extracting tar----"
-echo "tar -xvf wavity-distribution-1.0.0-SNAPSHOT.tar"
-tar -xvf wavity-distribution-1.0.0-SNAPSHOT.tar
-cd wavity-distribution-1.0.0-SNAPSHOT
+echo "tar -xvf wavity-distribution-2.0.0-SNAPSHOT.tar"
+tar -xvf wavity-distribution-2.0.0-SNAPSHOT.tar
+cd wavity-distribution-2.0.0-SNAPSHOT
 echo "----running development task on cloud deployment----"
 echo "gradle --refresh-dependencies -s development"
 gradle --refresh-dependencies -s development
--- run.sh
+++ run.sh
@@ -4,11 +4,13 @@ START=$(date +%s.%N)
 export oneteamrepo=/home/aman/data/oneteam
 export cloudrepo=/home/aman/data/cloud
 
-bin/setIp.sh && bin/stopPostgresql.sh && bin/buildStartDockerImages.sh && bin/buildDistributions.sh 
-
+bin/setIp.sh && bin/stopPostgresql.sh && bin/startDockerImages.sh && bin/buildDistributions.sh 
 echo "restarting scim and cas containers" 
 sleep 5
 echo "docker restart devapps1scim1; docker restart devapps1cas1" && docker restart devapps1scim1 && docker restart devapps1cas1
-
+echo "copied tenant to clip board"
+less bin/myTenant | xclip -sel clip
+echo "go to self.wavity.net/doc"
+xdg-open https://self.wavity.net/scim/doc &
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START")
